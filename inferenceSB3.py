0dfe0514c4915643c9a01032fab5c51f0455ea39
AIIPA09 <mihir2201k@gmail.com>
2024-04-20 00:27:55 -0700

    Major - File for loading inference and running trained weights and running a game of present version of Monopoly. INSTEAD of random, testing can happen on this.

---
+++ inferenceSB3.py
@@ -0,0 +1,53 @@
+from stable_baselines3 import PPO
+import os
+from monopoly.envs.monopoly_env2 import MonopolyEnv2
+import time
+
+models_dir = "models/"  # Update this path to the directory where your models are saved
+
+env = MonopolyEnv2(4, 2, 2, 200, './city1.csv')
+env.reset()
+
+# Load the saved model
+model_name = "1713580378/70000.zip"
+model = PPO.load(os.path.join(models_dir, model_name), env)
+
+# Run inference
+for _ in range(1):  # Adjust the number of episodes you want to run inference on
+    obs, _ = env.reset()
+    done = False
+    trunc = False
+    # while not done:
+    #     action, _ = model.predict(obs, deterministic=True)
+    #     obs, reward, done, _, _ = env.step(action)
+
+    while not done: # not done:
+        if trunc == True:
+            print(f"Episode failed")
+            break
+        print(f"Current_player: {env.current_player.num}")
+        print(f"Position before roll: {env.current_player.pos}")
+        random_action = env.action_space.sample()
+        print("action", random_action)
+
+        obs, reward, done, trunc, info = env.step(random_action)
+        print(f"Roll: {env.roll_val}")
+        print(f"Position after roll: {env.current_pos}")
+        # if reward != 0.:
+        print('state', [format(num, '.2f') for num in obs])
+        print('reward', reward)
+        print(info)
+        owner = []
+        worths = []
+        for city in env.board:
+            if city.owner != None:
+                owner_num = city.owner.num
+            else:
+                owner_num = 0
+            owner.append([city.name, owner_num])
+        for player in env.players:
+            worths.append([player.num, player.money])
+        print(owner)
+        print(worths)
+        print()
+    print(f"Episode reward: {env.reward}, Episode length: {env.episode_length}")
